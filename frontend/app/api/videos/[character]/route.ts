58b10d85cdbf5f3622de9b8bd9f156dd7db6b2c6
taisei08 <829taisei829@gmail.com>
2024-09-08 19:33:28 +0900

    fix: offsetを0に設定

--- frontend/app/api/videos/[character]/route.ts
+++ frontend/app/api/videos/[character]/route.ts
@@ -3,11 +3,10 @@ import { db } from "@/lib/firebase/firebase";
 
 export async function GET(res: NextResponse, { params }: { params: { character: string } }) {
   const { character } = params;
-  const offset = 50
+  const offset = 0
   const videosRef = db.collection(`videos/${character}/information`);
   const q = videosRef.orderBy("uploaded_at", "desc").offset(offset).limit(50);
   const querySnapshot = await q.get();
-
   if (!querySnapshot.empty) {
     const videosData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     return NextResponse.json(videosData, { status: 200 });
